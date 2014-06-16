250ec326bd23ff5a6942b36eac7632eed035d957
Takehiro Yoshihama <cloudq9team3@gmail.com>
2014-06-16 14:26:05 +0900

    初期化

--- actions.php
+++
@@ -1,6 +0,0 @@
-<?php
-$m = new MongoClient( 'mongodb://localhost:48103');
-$cursor = $m->tem->users->find( array( 'name' => 'marat'));
-$result = $cursor->getNext();
-echo json_encode( $result); 
-?>
